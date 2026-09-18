5457a34783443dca28de2a5d3365040c9df150ad
Deafinicio <deafinicio@users.noreply.github.com>
2026-09-18 13:31:43 +0300

    Align warning fence with the hazardous route boundary

--- js/stages/stage03.js
+++ js/stages/stage03.js
@@ -28,9 +28,9 @@ export const stage03 = {
     world.addBench(5.6, -18, Math.PI / 2);
 
     this.sign = world.addMineWarningSign(1.2, 0.7, -0.08);
-    world.addWarningFence(0.1, -2, 7.4, 0);
-    world.addWarningFence(4.1, -10.5, 17, Math.PI / 2);
-    world.addWarningFence(0.2, -19, 7.7, 0);
+    world.addWarningFence(4, -2, 7.5, 0);
+    world.addWarningFence(7.7, -10.5, 17, Math.PI / 2);
+    world.addWarningFence(4, -19, 7.5, 0);
 
     world.addMine(2.2, -6.2, {
       rotation: 0.35,
